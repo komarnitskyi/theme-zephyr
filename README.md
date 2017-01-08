3ad966f8120611d18e508229ed7a0e36e32765bb
Andrii Komarnitskyi <ticapac@gmail.com>
2017-01-08 15:25:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,43 @@
+<img src="https://cdn.rawgit.com/oh-my-fish/oh-my-fish/e4f1c2e0219a17e2c748b824004c8d0b38055c16/docs/logo.svg" align="left" width="144px" height="144px"/>
+
+#### zephyr
+> A theme for [Oh My Fish][omf-link].
+
+[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
+[![Fish Shell Version](https://img.shields.io/badge/fish-v2.2.0-007EC7.svg?style=flat-square)](https://fishshell.com)
+[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
+
+<br/>
+
+
+## Install
+
+```fish
+$ omf install zephyr
+```
+
+
+## Features
+
+* Displays git information in the command prompt when available.
+* Displays node version when available
+
+
+## Screenshot
+
+<p align="center">
+<img src="/screenshot.png">
+</p>
+
+
+# License
+
+[MIT][mit] © [Andrii Komarnitskyi][author] et [al][contributors]
+
+
+[mit]:            https://opensource.org/licenses/MIT
+[author]:         https://github.com/komarnitskyi
+[contributors]:   https://github.com/komarnitskyi/theme-zephyr/graphs/contributors
+[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+
+[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
---
+++ fish_prompt.fish
@@ -0,0 +1,77 @@
+# name: prfx
+
+set set_cyan (set_color -o cyan)
+set set_yellow (set_color -o yellow)
+set set_red (set_color -o red)
+set set_blue (set_color -o blue)
+set set_green (set_color -o green)
+set set_magenta (set_color magenta)
+set set_normal (set_color normal)
+
+set zephyr_PROMPT_SYMBOL "$set_green➜ $set_normal"
+
+# node
+set zephyr_NODE_SYMBOL "$set_green⬢ $set_normal"
+
+# git
+
+set zephyr_GIT_CLEAN "$set_green✔︎$set_normal"
+set zephyr_GIT_DIRTY "$set_red✗$set_normal"
+
+# git
+
+function _is_git_dirty
+   echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+# node
+function _get_node_version
+  echo (command node -v)
+end
+
+function fish_prompt
+  set -l last_status $status
+
+
+  if test $last_status = 0
+      set status_indicator "$set_green✔︎ "
+  else
+      set status_indicator "$set_red✗ "
+  end
+
+
+  set -l cwd $set_cyan(pwd)
+
+  if [ (_get_node_version) ]
+    set -l node_version (_get_node_version)
+    set node_info " $zephyr_NODE_SYMBOL $set_green$node_version$set_normal"
+  end
+
+
+
+  if [ (_git_branch_name) ]
+
+    if [ (_is_git_dirty) ]
+      set git_status "$zephyr_GIT_DIRTY"
+      else
+      set git_status "$zephyr_GIT_CLEAN"
+    end
+
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$set_normal ( $set_red$git_branch$set_normal $git_status )"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$set_normal ( $set_magenta$git_branch$set_normal $git_status )"
+    end
+
+  end
+
+  # Notify if a command took more than 5 minutes
+  if [ "$CMD_DURATION" -gt 300000 ]
+    echo The last command took (math "$CMD_DURATION/1000") seconds.
+  end
+  echo ''
+  echo -s $status_indicator $cwd $git_info $normal '' $node_info ''
+  echo -s $zephyr_PROMPT_SYMBOL ' '
+end
